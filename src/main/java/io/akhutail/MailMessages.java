a328ab9a4c7ddd728061e8a16eff3a60aba6678d
Abhishek Chaudhary <abhishekkhutail@gmail.com>
2022-09-11 18:16:28 +0530

    setup including github oauth2 and cassandra connectivity setup

---
+++ src/main/java/io/akhutail/MailMessages.java
@@ -0,0 +1,34 @@
+package io.akhutail;
+
+import java.nio.file.Path;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@SpringBootApplication
+@RestController
+public class MailMessages {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailMessages.class, args);
+	}
+
+	@RequestMapping("/user")
+	public Mono<String> user(@AuthenticationPrincipal Mono<OAuth2User> principal) {
+		return principal.map(val -> val.getAttribute("login"));
+	}
+	
+	@Bean
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties){
+		Path bundle = astraProperties.getSecureConnectBundle().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(bundle);
+	}
+
+}
