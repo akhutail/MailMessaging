b5e93641a8402ef50a7b3e07dcef7e20d2c551f7
Abhishek Chaudhary <abhishekkhutail@gmail.com>
2023-02-18 03:06:42 +0530

    changed to use local cassandra db running in docker instead of astra

--- src/main/java/io/akhutail/mail/MailMessages.java
+++ src/main/java/io/akhutail/mail/MailMessages.java
@@ -1,15 +1,13 @@
 package io.akhutail.mail;
 
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
-
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
-import org.springframework.context.annotation.Bean;
+
 
 import com.datastax.oss.driver.api.core.uuid.Uuids;
 
@@ -19,7 +17,6 @@ import io.akhutail.mail.emails.emailsByUserFolder.EmailsByUserFolder;
 import io.akhutail.mail.emails.emailsByUserFolder.EmailsByUserFolderRepo;
 import io.akhutail.mail.folders.Folder;
 import io.akhutail.mail.folders.FolderRepository;
-import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class MailMessages {
@@ -29,12 +26,6 @@ public class MailMessages {
 	@Autowired EmailsRepo emailsRepo;
 	public static void main(String[] args) {
 		SpringApplication.run(MailMessages.class, args);
-	}
-	
-	@Bean
-	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties){
-		Path bundle = astraProperties.getSecureConnectBundle().toPath();
-		return builder -> builder.withCloudSecureConnectBundle(bundle);
 	}
 
 	@PostConstruct
@@ -51,7 +42,7 @@ public class MailMessages {
 		emailsByUserFolderRepo.save(new EmailsByUserFolder("akhutail", "Inbox", id2, "sender2", "subject2", true ));
 		emailsByUserFolderRepo.save(new EmailsByUserFolder("akhutail", "Sent", id3, "sender3", "subject3", false ));
 
-		List<String> listOfIds = new ArrayList<String>();
+		List<String> listOfIds = new ArrayList<>();
 		listOfIds.add("user11"); listOfIds.add("user12");
 		
 		emailsRepo.save(new EmailsById(id1, "sender1",  listOfIds, "subject1", 
